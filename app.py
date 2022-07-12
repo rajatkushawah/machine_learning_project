c4a259a584559330aa31d246376f2276c96b332e
rajat kushawah <rajatkushawah@gmail.com>
2022-07-12 23:39:38 +0530

    logging and exception is completed

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
-
+from housing.logger import logging
+from housing.exception import HousingException
 app=Flask(__name__)
 
 
 @app.route("/",methods=['GET' , 'POST'])
 def index():
+    try:
+        raise Exception("we are testing custome exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
     return"starting machine learning project"
 
 
---
+++ housing/exception/__init__.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+class HousingException (Exception):
+
+    def __init__(self,error_message:Exception,error_detail:sys):
+        super().__init__(error_message)
+
+        self.error_message=error_message
+
+
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
+        _,_,exec_tb = error_detail.exc_info()
+
+        line_number = exec_tb.tb_frame.f_lineno
+        file_name = exec_tb.tb_frame.f_code.co_filename
+        error_message = f"error occured in script: [{file_name}] at line no.: [{line_number}] error_message: [{error_message}]"
+
+        return error_message
+
+    def __str__(self):
+        return self.error_message
+
+    def __repr__(self)->str:
+        return HousingException.__name__.str()
